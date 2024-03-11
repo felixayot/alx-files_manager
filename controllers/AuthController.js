627c90f083ae79dcf322491748f766167277e61a
thinktank01 <felixayot@gmail.com>
2024-03-12 00:34:22 +0300

    Update /connect &/users/me to fix module import errors

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,40 +1,42 @@
 // Authentication controller
+const sha1 = require('sha1');
 const uuid = require('uuid').v4;
 const dbClient = require('../utils/db');
 
 class AuthController {
   // GET /connect
-  static getConnect(req, res) {
-    const Authorization = req.header('Authorization');
-    if (!Authorization) {
+  static async getConnect(req, res) {
+    const Authorization = req.header('Authorization') || '';
+    const base64Credentials = Authorization.split(' ')[1];
+    if (!base64Credentials) {
       return res.status(401).send({ error: 'Unauthorized' });
     }
-    const base64Credentials = Authorization.split(' ')[1];
-    const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('utf-8');
     const [email, password] = credentials.split(':');
     if (!email || !password) {
       return res.status(401).send({ error: 'Unauthorized' });
     }
-    const user = dbClient.usersCollection.findOne({ email, password });
+    const hashedPassword = sha1(password);
+    const user = dbClient.usersCollection.findOne({ email, password: hashedPassword });
     if (!user) {
       return res.status(401).send({ error: 'Unauthorized' });
     }
     const token = uuid();
+    const key = `auth_${token}`;
+    const validFor = 24;
+    await dbClient.redisClient.set(key, user._id.toString(), validFor * 3600);
     dbClient.usersCollection.update({ email, password }, { $set: { token } });
     return res.status(200).send({ token });
   }
 
   // GET /disconnect
-  static getDisconnect(req, res) {
-    const token = req.header('X-Token');
-    if (!token) {
+  static async getDisconnect(req, res) {
+    const { userId, key } = await dbClient.redisClient.get(`auth_${req.header('X-Token')}`);
+    if (!userId || !key) {
       return res.status(401).send({ error: 'Unauthorized' });
     }
-    const user = dbClient.usersCollection.findOne({ token });
-    if (!user) {
-      return res.status(401).send({ error: 'Unauthorized' });
-    }
-    dbClient.usersCollection.update({ token }, { $set: { token: '' } });
+    await dbClient.redisClient.del(key);
+
     return res.status(204).send();
   }
 }
